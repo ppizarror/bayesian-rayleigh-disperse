d076f57d6d293eec57f0aa394191737fe71ea826
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2017-05-29 05:39:58 +0100

    Se agregan cambios nuevos, creado main

--- rms_error.m
+++
@@ -1,3 +0,0 @@
-function [rms_error]=rms_error(sig1,sig2)
-N=length(sig1);
-rms_error=roundn(sqrt(1/N*sum((sig1-sig2).^2))/sqrt(1/N*sum(sig1.^2))*100,-2);
---
+++ run_dukf.m
@@ -0,0 +1,7 @@
+function [ output_args ] = run_dukf( input_args )
+%RUN_DUKF Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
